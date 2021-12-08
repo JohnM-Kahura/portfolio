0210e9f319818c3cac5476ec829c9ec3a4ce908f
JohnM-Kahura <johnmungai392@gmail.com>
2021-12-08 17:46:00 +0300

    wednesday out

--- src/components/Portfolio/PortFolio.jsx
+++ src/components/Portfolio/PortFolio.jsx
@@ -1,16 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import PortFolioList from '../portfoliolist/PortFolioList'
 import "./portfolio.scss"
+import {
+    featuredPortfolio,
+    webPortfolio,
+    mobilePortfolio,
+
+    scriptsPortfolio,
+} from '../../data'
 function PortFolio() {
+    const [selected, setselected] = useState("featured")
+    const [data, setdata] = useState([])
+    const list = [
+        {
+            id: "featured",
+            title: "Featured",
+        },
+        {
+            id: "web",
+            title: "Web Apps",
+
+        },
+        {
+            id: "mobile",
+            title: "Mobile Apps",
+
+        },
+        {
+            id: "scripts",
+            title: "Python Scripts",
+
+        },
+    ]
+
+    useEffect(() => {
+        switch (selected) {
+            case "featured":
+                setdata(featuredPortfolio)
+                break;
+            case "web":
+                setdata(webPortfolio)
+                break;
+            case "mobile":
+                setdata(mobilePortfolio)
+                break;
+            case "scripts":
+                setdata(scriptsPortfolio)
+                break;
+            default:
+                setdata(featuredPortfolio)
+                break;
+        }
+    }, [selected])
     return (
-        <div className="portfolio" id ="portfolio">
+        <div className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
             <ul>
-                <li>Featured</li>
-                <li>Mobile Apps</li>
-                <li>Web Apps</li>
-                <li>Python Scripts</li>
+                {list.map(item => (
+                    <PortFolioList title={item.title} active={selected === item.id} id={item.id} setselected={setselected} />
+                ))}
             </ul>
-        </div> 
+            <div className="container">
+                {data.map(d => (
+                    <div className="item">
+                        <img src={d.img} alt="" />
+                        <h3>{d.title}</h3>
+                    </div>
+
+                ))}
+
+
+            </div>
+        </div>
     )
 }
 
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./work.scss"
-
+import {workData} from "../../data"
 export default function Work() {
+
+    const [currentSlider, setcurrentSlider] = useState(0);
+   const handleArrowClick=(direction)=>{
+direction==="left"? setcurrentSlider(currentSlider>0?currentSlider-1:2):
+setcurrentSlider(currentSlider< workData.length -1?currentSlider+1:0)
+
+   }
     return (
         <div className="work" id="work">
-            
+            <div className="slider" style={{
+                transform:`translateX(-${currentSlider *100}vw)`
+            }}>
+                {workData.map(d=>(
+
+                <div className="container">
+                    <div className="item">
+                         <div className="left">
+                             <div className="leftContainer">
+                                 <div className="imageContainer">
+                                     <img src={d.icon} alt="" />
+                                 </div>
+                                 <h2>{d.title}</h2>
+                                 <p>{d.desc}
+                                     </p>
+                                     <span>Projects</span>
+                             </div>
+                         </div>
+                         <div className="right">
+
+                         </div>
+                    </div>
+                    </div>
+                    ))}
+                    </div>
+                    <img src="assets/arrow.png" className="arrow left " alt="" onClick={()=>handleArrowClick("left")}/>
+                    <img src="assets/arrow.png" className="arrow right " alt="" onClick={()=>handleArrowClick("right")} />
         </div>
     )
 }
