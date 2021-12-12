3ca6bca0de22fd969c5ef208ace292f00fb44960
JohnM-Kahura <johnmungai392@gmail.com>
2021-12-12 08:48:52 +0300

    mobile view completed

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-// import {Person,Mail} from 'material-ui/icons';
+
 import Mail from '@mui/icons-material/Email';
 import Person from '@mui/icons-material/Person';
 import GitHubIcon from '@mui/icons-material/GitHub'; 
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 
 import "./topbar.scss"
@@ -11,18 +12,22 @@ function TopBar({menuOpen,setmenuOpen}) {
         <div className={"topbar "+(menuOpen &&"active")}>
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro" className="logo" >Mungai.</a>
-                    <div className="itemContainer">
+                    <a href="#intro" className="logo" >JM.</a>
+                    <div className="itemContainer phone">
                         <Person className="icon"/>
                         <span>0795388701</span>
                     </div>
-                    <div className="itemContainer">
+                    <div className="itemContainer email">
                         <Mail className="icon"/>
                         <span>johnmungai392@gmail.com</span>
                     </div>
-                    <div className="itemContainer">
+                    <div className="itemContainer github">
                         <GitHubIcon className="icon"/>
                         <span><a href="https://github.com/JohnM-Kahura"target="_blank" rel="noopener noreferrer">GitHub</a></span>
+                    </div>
+                    <div className="itemContainer">
+                        <TwitterIcon className="icon twitter"/>
+                        <span><a href="https://twitter.com/JMKahura"target="_blank" rel="noopener noreferrer">Twitter</a></span>
                     </div>
                 </div>
                 <div className="right">
