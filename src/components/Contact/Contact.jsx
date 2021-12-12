4953bf04ca7cc38e993e956c694142d70545e655
JohnM-Kahura <johnmungai392@gmail.com>
2021-12-12 07:34:00 +0300

    mobile scss

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./contact.scss"
 function Contact() {
+    const [message, setmessage] = useState(false)
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        setmessage(true)
+    }
     return (
         <div className="contact" id="contact">
-            
+            <div className="left">
+                <img src="assets/shake.svg" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact</h2>
+                <form action="" onSubmit={handleSubmit}>
+                    <input type="text" placeholder='Email'/>
+                    <textarea name="" placeholder='Message'  cols="30" rows="10"></textarea>
+                <button type='submit'>Submit</button>
+                {message&& <div className='message'>
+                    <span>Thanks for contacting me I will reply ASAP </span>
+                    </div>}
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import "./testimonials.scss"
+import {testimonialsData} from "../../data"
 function Testimonials() {
+    const openSocialTab = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+      }
     return (
-        <div className="testimonials"id="testimonials">
-            
+        <div className="testimonials" id="testimonials">
+            <h1> Testimonials</h1>
+            <div className="container">
+                {testimonialsData.map(d=>(
+
+                    <div className={d.featured ? "card featured":"card"}>
+                    <div className="top">
+                        <img src="assets/right-arrow.png" className='left' alt="" />
+                        <img src={d.img} alt=""  className='user' />
+                      
+                        <img src={d.icon} onClick={()=> openSocialTab(d.social)} className='right' alt="" />
+                    </div>
+                    <div className="center">
+                        {d.desc}
+                    </div>
+                    <div className="bottom">
+                        <h1>{d.name}</h1>
+                        <h4>{d.title}</h4>
+                    </div>
+                </div>
+            ))}
+            </div>
         </div>
     )
 }
